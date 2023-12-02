9aa2834ffe27807578801e329faf3c918ee884ce
moldovexc <alexanderparpulansky@gmail.com>
2023-12-02 22:54:36 +0200

    updated queries to match new data

--- query.sql
+++ query.sql
@@ -1,4 +1,4 @@
---1
+--1 find reservations checking in at current date
 SELECT
     r.id AS reservation_number,
     rm.room_number,
@@ -17,35 +17,36 @@ FROM
 WHERE
     DATE(check_in_date) = CURRENT_DATE;
 
---3
+--3 most reserved periods the rooms in the periods and the number of reservations for them
  WITH MostReservedPeriod AS (
-     SELECT
-         r.check_in_date AS period_start,
-         r.check_out_date AS period_end
-     FROM
-         public.reservation r
-     GROUP BY
-         r.check_in_date, r.check_out_date
-     ORDER BY
-         COUNT(r.id) DESC
-     LIMIT 1
- )
+      SELECT
+          r.check_in_date AS period_start,
+          r.check_out_date AS period_end
+      FROM
+          public.reservation r
+      GROUP BY
+          r.check_in_date, r.check_out_date
+      ORDER BY
+          COUNT(r.id) DESC
+      LIMIT 1
+  )
 
 
- SELECT
-     r.room_id AS reserved_room,
-     m.period_start,
-     m.period_end,
-     COUNT(r.id) AS number_of_reservations
- FROM
-     public.reservation r
- JOIN
-     MostReservedPeriod m ON r.check_in_date = m.period_start
-                         AND r.check_out_date = m.period_end
- GROUP BY
-     r.room_id, m.period_start, m.period_end
- ORDER BY
-     number_of_reservations DESC;
+  SELECT
+      rm.room_number,
+      m.period_start,
+      m.period_end,
+      COUNT(r.id) AS number_of_reservations
+  FROM
+      public.reservation r
+  JOIN
+      MostReservedPeriod m ON r.check_in_date = m.period_start
+                          AND r.check_out_date = m.period_end
+  JOIN public.room rm ON r.room_id = rm.id
+  GROUP BY
+      rm.room_number, m.period_start, m.period_end
+  ORDER BY
+      number_of_reservations DESC;
 
 
 --4 terminate reservation
